ff0e9afe5b13f3cc4a83ef5b55ba7c30dc2a97d8
ElvinSadikhov <83132276+ElvinSadikhov@users.noreply.github.com>
2021-05-09 19:48:24 +0400

    expenditure.py
    Expenditure list which works with excel using openpyxl

--- expenditures.py
+++ expenditures.py
@@ -1,97 +1,83 @@
-'''
-sheet=book.active
-sheet['A1'] = 56
-sheet['A2'] = 43
-now = date.today()
-sheet['A3'] = now
-    
-sheet = book.active
-rows = [
-        [88, 46, 90],
-        [89, 38, 12],
-        [23, 59, 78],
-        [56, 0, 98],
-        [24, 0, 43],
-        [34, 0, 67]
-    ]
-rows = ["1","2","3"]
-sheet.append(rows)
-book.save('expenses.xlsx')
-
-book=load_workbook("expenses.xlsx")
-sheet=book.active
-a=sheet["A1"]
-print(sheet["A1"].value)
-
-'''
 from openpyxl import *
 from openpyxl.styles import *
 from datetime import date
 from time import sleep
 import os
 
-def createTable():
-    '''Creates an excel file and types formatted titles'''
-    book = Workbook()
-    sheet=book.active
-    sheet["A1"],sheet["B1"],sheet["C1"],sheet["D1"]=\
+def CreateOrOpenTable():
+    '''Creates an excel file and types formatted titles or opens an existing one'''
+    try:
+        book=load_workbook("expenses.xlsx")
+        book.save("expenses.xlsx")
+    except:
+        book = Workbook()
+        sheet=book.active
+        sheet["A1"],sheet["B1"],sheet["C1"],sheet["D1"]=\
             "Num","Purpose","Cost","Date"
-    for cell in ["A1","B1","C1","D1"]:
-        sheet[cell].font=Font(bold=True)
-        sheet[cell].alignment=Alignment(horizontal="center",vertical="center")
-    book.save("expenses.xlsx")
+        for cell in ["A1","B1","C1","D1"]:
+            sheet[cell].font=Font(bold=True)
+            sheet[cell].alignment=Alignment(horizontal="center",vertical="center")
+        book.save("expenses.xlsx")
     return book
 
-def appendTable(lst):##
-    sheet = book.active
-    sheet['A1'] = "fe3"
-    sheet.cell(row=3, column=2).value = "fdf"
+def appendTable(lst):
+    book=load_workbook('expenses.xlsx')
+    sheet=book.active
+    if sheet.max_row==1:
+        i=0
+    else:
+        i=sheet.max_row-1
+    for line in lst:
+        i+=1
+        line=[i]+line[:]
+        sheet.append(line)
     book.save('expenses.xlsx')
 
-def checkDate(date):
-    d,m,y=int(date[0]),int(date[1]),int(date[2])
-    if date(y,m,d)>date.today():
-        return False
-    if date[1] in ["01","03","05","07","08","10","12"]:
-        if int(date[0])>31:
-            return False
-    elif date[1] in ["04","06","09","11"]:
-        if int(date[0])>30:
-            return False
-    else:
-        if int(date[2])%4==0:
-            if int(date[0])>29:
-                return False
-        else:
-            if int(date[0])>28:
-                return False
-    return True
-
-
+def checkDate(Date):
+    d,m,y=int(Date[0]),int(Date[1]),int(Date[2])
+    try:
+        return date(y,m,d)<=date.today()
+    except:
+        return False    
+def printTable():
+    book=load_workbook('expenses.xlsx')
+    sheet=book.active
+    max_col=int(sheet.max_column)
+    max_row=int(sheet.max_row)
+    print("\n")
+    for Row in range(1,max_row+1):
+        for Col in range(1,max_col+1):
+            print("{:20s}".format(str(sheet.cell(row=Row,column=Col).value)),end="")
+        print()
+    book.save("expenses.xlsx")
     
-
 def main():
-    book=createTable()
+    global book
+    print("[LIST OF EXPENCES]\n")
+    book=CreateOrOpenTable()
     info=[]
     while True:
-        print("LIST OF EXPENCES")
         purpose=input("Type a purpose/reason for expenditure:").strip()
-        cost=input("Type a cost of it:(in dollars)")
         while True:
-            date=input("Type date of it:(e.g DD.MM.YYYY)").split(".")#it is a list
-            if len(date[0])!=2 or len(date[1])!=2 or len(date[2])!=4:
-                print("The given date is incorrect!")
-            else:
+            cost=input("Type a cost of it:(in dollars)")
+            try:
+                cost=float(cost)
                 break
-            
-        info.append([purpose,cost,date])
+            except:
+                temp=0
+        while True:
+            Date=input("Type date of it:(e.g DD.MM.YYYY)").split(".")
+            if checkDate(Date):
+                    break
+        info.append([purpose,cost,str(date(int(Date[2]),int(Date[1]),int(Date[0])))[:10]])
         check=input("Do you want to add something?:(split line if NO!)")
-        sleep(1.2)
-        os.system("cvd")
         if len(check)==0:
+            sleep(1.2)
+            os.system("cls")
             break
+    appendTable(info)
+    printTable()
+    sleep(10000)
     
-
-
 if __name__=="__main__":
     main()
